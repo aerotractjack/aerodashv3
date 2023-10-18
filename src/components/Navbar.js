18e39ab06be6ca864c8add960b45a6637202d7d5
Yijia Chen <kevinc110604@gmail.com>
2023-10-17 21:43:00 -0700

    [new] Add Navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,36 @@
-import { Flex } from "@chakra-ui/react";
-
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import aerotract_navbar_logo from '../assets/aerotract_navbar_logo.png';
+import { Navbar as BootstrapNavbar, NavDropdown, Nav } from 'react-bootstrap';
+import { createTables, viewTables } from '../config.js'
 
 const Navbar = () => {
-  return (
-    <>
-    <Flex
-      bg="#7c8a94"
-      p={4}
-      zIndex={1}
-      position="sticky"
-      top={0}
-    >
-
-    </Flex>
-    </>
-  );
+    return (
+        <BootstrapNavbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Link to="/" className="navbar-brand">
+                <img src={aerotract_navbar_logo} alt="Logo" style={{ width: '150px', marginRight: '10px' }} />
+            </Link>
+            <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+            <BootstrapNavbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavDropdown title="Create" id="basic-nav-dropdown">
+                        {Object.keys(createTables).map((key) => (
+                            <NavDropdown.Item as={NavLink} key={key} to={`/create/${createTables[key]}`}>
+                                {key}
+                            </NavDropdown.Item>
+                        ))}
+                    </NavDropdown>
+                    <NavDropdown title="View" id="basic-nav-dropdown">
+                        {Object.keys(viewTables).map((key) => (
+                            <NavDropdown.Item as={NavLink} key={key} to={`/view/${viewTables[key]}`}>
+                                {key}
+                            </NavDropdown.Item>
+                        ))}
+                    </NavDropdown>
+                </Nav>
+            </BootstrapNavbar.Collapse>
+        </BootstrapNavbar>
+    );
 };
 
 export default Navbar;
